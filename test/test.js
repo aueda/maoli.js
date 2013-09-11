aeda6fe5bf636dee59c645b7e1427bb250f50962
Adriano Ueda <adriano.ueda@gmail.com>
2013-09-10 23:01:26 -0300

    Cleaned up test code
    Code for unit tests were cleaned and refactored according to JSLint.

--- test/test.js
+++ test/test.js
@@ -1,70 +1,75 @@
-(function (maoli) {
+(function (window) {
 
     "use strict";
 
-    var looseValidCnpj = "63943315000192",
-        looseInvalidCnpj = "32343315/000134",
-        strictValidCnpj = "63.943.315/0001-92",
-        strictInvalidCnpj = "32.343.315/0001-34";
+    var maoli = window.Maoli,
+        test = window.QUnit.test,
+        ok = window.QUnit.ok,
+        runCnpjTests = function () {
 
-    test("Validate Returns True If CNPJ Is Valid", function () {
-        ok(maoli.Cnpj.validate(looseValidCnpj));
-    });
+            test("Validate Returns True If CNPJ Is Valid", function () {
+                ok(maoli.Cnpj.validate("63943315000192"));
+            });
 
-    test("Validate Returns False If CNPJ Is Invalid", function () {
-        ok(!maoli.Cnpj.validate(looseInvalidCnpj));
-    });
-    
-    test("Validate Returns False If CNPJ Contains Invalid Chars", function () {
-        ok(!maoli.Cnpj.validate("714o256s8"));
-    });
-    
-    test("Validate Returns False If CNPJ Is Valid But Not Strict", function () {
-        ok(!maoli.Cnpj.validate(looseValidCnpj, "strict"));
-    });
+            test("Validate Returns False If CNPJ Is Invalid", function () {
+                ok(!maoli.Cnpj.validate("32343315/000134"));
+            });
 
-    test("Validate Returns True If CNPJ Is Valid And Strict", function () {
-        ok(maoli.Cnpj.validate(strictValidCnpj, "strict"));
-    });
+            test("Validate Returns False If CNPJ Contains Invalid Chars", function () {
+                ok(!maoli.Cnpj.validate("714o256s8"));
+            });
 
-    test("Validate Returns False If CNPJ Is Half Punctuated And Valid And Loose", function() {
-        ok(maoli.Cnpj.validate("63.9433150001-92", "loose") === false);
-    });
+            test("Validate Returns False If CNPJ Is Valid But Not Strict", function () {
+                ok(!maoli.Cnpj.validate("63943315000192", "strict"));
+            });
 
-}(window.Maoli));
+            test("Validate Returns True If CNPJ Is Valid And Strict", function () {
+                ok(maoli.Cnpj.validate("63.943.315/0001-92", "strict"));
+            });
 
-(function (maoli) {
+            test("Validate Returns False If CNPJ Is Half Punctuated And Valid And Loose", function () {
+                ok(maoli.Cnpj.validate("63.9433150001-92", "loose") === false);
+            });
+        };
+
+    runCnpjTests();
+
+}(this));
+
+(function (window) {
 
     "use strict";
 
-    var looseValidCpf = "71402565860",
-        looseInvalidCpf = "82513676932",
-        strictValidCpf = "714.025.658-60",
-        strictInvalidCpf = "825.136.769-32";
+    var maoli = window.Maoli,
+        test = window.QUnit.test,
+        ok = window.QUnit.ok,
+        runCpfTests = function () {
 
-    test("Validate Returns True If CPF Is Valid", function () {
-        ok(maoli.Cpf.validate(looseValidCpf));
-    });
-    
-    test("Validate Returns False If CPF Is Invalid", function () {
-        ok(!maoli.Cpf.validate(looseInvalidCpf));
-    });
+            test("Validate Returns True If CPF Is Valid", function () {
+                ok(maoli.Cpf.validate("71402565860"));
+            });
 
-    test("Validate Returns False If CPF Contains Invalid Chars", function () {
-        ok(!maoli.Cpf.validate("714o256s8"));
-    });
+            test("Validate Returns False If CPF Is Invalid", function () {
+                ok(!maoli.Cpf.validate("82513676932"));
+            });
 
-    test("Validate Returns False If CPF Is Valid But Not Strict", function () {
-        ok(!maoli.Cpf.validate(looseValidCpf, "strict"));
-    });
+            test("Validate Returns False If CPF Contains Invalid Chars", function () {
+                ok(!maoli.Cpf.validate("714o256s8"));
+            });
 
-    test("Validate Returns True If CPF Is Valid And Strict", function () {
-        ok(maoli.Cpf.validate(strictValidCpf, "strict"));
-    });
+            test("Validate Returns False If CPF Is Valid But Not Strict", function () {
+                ok(!maoli.Cpf.validate("71402565860", "strict"));
+            });
 
-    test("Validate Returns False If CPF Is Half Punctuated And Valid And Loose", function() {
-        ok(maoli.Cpf.validate("714.025.65860", "loose") === false);
-    });
+            test("Validate Returns True If CPF Is Valid And Strict", function () {
+                ok(maoli.Cpf.validate("714.025.658-60", "strict"));
+            });
 
-}(window.Maoli));
+            test("Validate Returns False If CPF Is Half Punctuated And Valid And Loose", function () {
+                ok(maoli.Cpf.validate("714.025.65860", "loose") === false);
+            });
+        };
 
+    runCpfTests();
+
+}(this));
