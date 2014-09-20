596941d218368e56b0658ada36acbe689c531938
Adriano Ueda <adriano.ueda@gmail.com>
2014-09-20 16:44:25 -0400

    Playing with Travis-CI.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,33 @@
 module.exports = function (grunt) {
 
-    'use strict';
+    "use strict";
 
+    // Execute QUnit unit tests
     grunt.loadNpmTasks('grunt-contrib-qunit');
 
+    // Minify files with Google Closure Compiler
+    grunt.loadNpmTasks('grunt-closure-compiler');
+
     grunt.initConfig({
+        'closure-compiler': {
+            frontend: {
+                js: 'maoli.js',
+                jsOutputFile: 'maoli.min.js',
+                createSourceMap: 'maoli.min.map',
+                sourceMapFormat: 'V3',
+                maxBuffer: 500,
+                options: {
+                    compilation_level: 'SIMPLE_OPTIMIZATIONS',
+                    language_in: 'ECMASCRIPT5_STRICT'
+                }
+            }
+        },
+
         qunit: {
             source: ["test/test.html"],
             minified: ["test/test-min.html"]
         }
     });
+
+    grunt.registerTask("default", "closure-compiler");
 }
