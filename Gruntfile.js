3d362ccdc43bbd906dcbf3c71ef436fdfa84db4a
Adriano Ueda <adriano.ueda@gmail.com>
2014-09-22 20:21:28 -0400

    Refactored the Gruntfile for JSLint compliance.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,49 @@
-module.exports = function (grunt) {
+/*globals
+    module
+*/
+
+(function () {
 
     "use strict";
 
-    // Execute QUnit unit tests
-    grunt.loadNpmTasks('grunt-contrib-qunit');
+    if (module === undefined) {
+        return;
+    }
 
-    // Minify files with Google Closure Compiler
-    grunt.loadNpmTasks('grunt-closure-compiler');
+    module.exports = function (grunt) {
 
-    grunt.initConfig({
-        'closure-compiler': {
-            frontend: {
-                closurePath: '.',
-                js: 'maoli.js',
-                jsOutputFile: 'maoli.min.js',
-                maxBuffer: 500,
-                options: {
-                    compilation_level: 'SIMPLE_OPTIMIZATIONS',
-                    language_in: 'ECMASCRIPT5_STRICT',
-                    create_source_map: 'maoli.min.map',
-                    source_map_format: 'V3'
+        // Execute QUnit unit tests
+        grunt.loadNpmTasks('grunt-contrib-qunit');
+
+        // Minify files with Google Closure Compiler
+        grunt.loadNpmTasks('grunt-closure-compiler');
+
+        grunt.initConfig({
+            'closure-compiler': {
+                frontend: {
+                    closurePath: '.',
+                    js: 'maoli.js',
+                    jsOutputFile: 'maoli.min.js',
+                    maxBuffer: 500,
+                    options: {
+                        compilation_level: 'SIMPLE_OPTIMIZATIONS',
+                        language_in: 'ECMASCRIPT5_STRICT',
+                        create_source_map: 'maoli.min.map',
+                        source_map_format: 'V3'
+                    }
                 }
+            },
+
+            qunit: {
+                source: ["test/test.html"],
+                minified: ["test/test-min.html"]
             }
-        },
+        });
 
-        qunit: {
-            source: ["test/test.html"],
-            minified: ["test/test-min.html"]
-        }
-    });
+        grunt.registerTask("minify", "closure-compiler");
 
-    grunt.registerTask("minify", "closure-compiler");
+        grunt.registerTask("test", "qunit");
 
-    grunt.registerTask("test", "qunit");
-
-    grunt.registerTask("default", ["minify", "test"]);
-}
+        grunt.registerTask("default", ["minify", "test"]);
+    };
+}());
