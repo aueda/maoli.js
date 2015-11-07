221385fda7163b8d4dc5c0d8b7d3bbfa5699fbc0
Adriano Ueda <adriano.ueda@gmail.com>
2015-11-07 10:59:46 -0500

    Added the jQuery adapter.

---
+++ jquery.validate.maoli.js
@@ -0,0 +1,57 @@
+﻿// ==ClosureCompiler==
+// @output_file_name jquery.validate.maoli.min.js
+// @compilation_level SIMPLE_OPTIMIZATIONS
+// ==/ClosureCompiler==
+
+/*jslint
+    this
+*/
+(function (window) {
+
+    "use strict";
+
+    var maoli = window.Maoli,
+
+        $ = window.jQuery,
+
+        validator = null,
+
+        addCepMethod = function () {
+
+            validator.addMethod("cep_validator", function (value, element) {
+
+                var punctuation = $(element).data("val-cep-punctuation") || "loose";
+
+                return maoli.Cep.validate(value, punctuation);
+            });
+
+            validator.unobtrusive.adapters.addBool('cep', 'cep_validator');
+        };
+
+    if (maoli === "undefined" || $ === "undefined" || $.validator === "undefined") {
+        return;
+    }
+
+    validator = $.validator;
+
+    addCepMethod();
+
+    validator.addMethod("cnpj_validator", function (value, element) {
+
+        var punctuation = $(element).data("val-cnpj-punctuation") || "loose";
+
+        return maoli.Cnpj.validate(value, punctuation);
+    });
+
+    validator.addMethod("cpf_validator", function (value, element) {
+
+        var punctuation = $(element).data("val-cpf-punctuation") || "loose";
+
+        return maoli.Cpf.validate(value, punctuation);
+    });
+
+    validator.unobtrusive.adapters.addBool('cnpj', 'cnpj_validator');
+
+    validator.unobtrusive.adapters.addBool('cpf', 'cpf_validator');
+
+}(this));
